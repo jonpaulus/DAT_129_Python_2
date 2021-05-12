34b7c7f7bc299b7793e84f57b80598a9605acdee
Jon Paulus <jpaul14@acd.ccac.edu>
2021-05-11 21:31:28 -0400

    Final comments made

--- Final.py
+++ Final.py
@@ -15,10 +15,15 @@ pro_publica_req = 'https://api.propublica.org/congress/v1/{}/senate/members.json
 
 #This is a wrapper function that requires the pro_publica_req variable, and the parameter is the session of congress that is desired (necessary for the request to work)
 # as well as the required header and propublica key required to authenticate the request by propublica
-#Required input:  desired session number in range of 101 to 117
+#Required input:  desired session number in range of 110 to 117 to be supplied by the user in the form of an integer
 #Output Api get request of specified session of Congress 
 def get_congress_session(number):
     return requests.get(pro_publica_req.format(number), headers = {"X-API-Key" : pro_publica_key})
+
+#This function places the the response.json for results and members, where in the actual response the desired data is.
+#Following this, the .csv is made based on the session of congress (supplied by user when the program is activated)
+#When the file is opened the headers are written (last name, first name, votes with party pct, votes against party pct and missed votes)
+#using the header names, it loops through the information and writes the columns based on the information with the key words (which are also the column names)
 
 def get_CSV_data(response):
     senators = response.json()['results'][0]['members']
@@ -30,18 +35,33 @@ def get_CSV_data(response):
         for senator in senators:
             writer.writerow(senator)
 
-#scatter plot functions
+#scatter plot functions 
+
+#Function is taking seniority information from ProPublica and creating a ranking system by taking the maximum seniority, 
+# subtracting each senators listed senority and assigning it 
+#a number.  Lowser the number the more senior the senator is.  This is used in the display_scatter_plots as the Y axis when called
 def calculate_senority(senators):
     seniority = [int (senator["seniority"]) for senator in senators]
     maximum = max (seniority)
     return [ maximum - i for i in seniority]
 
+#Function loops through obtained information extracting the data that represents the votes with party information to display 
+# on the X access when called in the display_scatter_plot function
+
 def calculate_votes_with_party(senators):
     return [float(senator["votes_with_party_pct"]) for senator in senators]
+
+#Function used by display_scatter_plot to create key on graph.  It loops through the obtained information 
+# and based on the party affiliation provided, assigns it a color to be used.
 
 def display_party_affiliation (senators):
     key = {"R": "red", "D" : "blue", "ID": "green"}
     return [key [senator["party"]] for senator in senators]
+
+
+#function to create scatter plot based on user input to obtain the .json information from ProPublica,
+#calculate_senorit, calculate_votes_with_party, and display_party_affiliation to generate a graph,
+#that plots each member of congress, color coded by party, on a field based on their seniority and how often they vote in line with their party
 
 def display_scatter_plot (response):
     senators = response.json()['results'][0]['members']
@@ -60,10 +80,13 @@ def display_scatter_plot (response):
 
 #line graph items
 
+#function that calculates the mean for Democrats, Republicans, and Independents (when available) that demonstrates how often each party votes togehter on average. 
+
 def calculate_votes_with_party_average(senators, party):
     filter_senators = [ senator for senator in senators if senator ["party"] ==party]
     return mean(calculate_votes_with_party(filter_senators)) if filter_senators else [0]
 
+#function that createss a line graph for each party by applying calculate_votes_with_party_average for each party for sessions 101 - 117
 
 def display_line_graph (response):
     congressional_sessions_range = range (101, 118)
@@ -77,7 +100,7 @@ def display_line_graph (response):
     item_1 = ax.bar(x - width, republican_mean, width, label='Republican', color="red")
     item_2 = ax.bar(x, democrat_mean, width, label='Democrat', color="blue")
     item_3 = ax.bar (x + width, independent_mean, width, label= "Independent", color="green")
-    plt.xlabel("Party")
+    plt.xlabel("Session of Congress")
     plt.ylabel("Average Vote with Party Percentage")
     plt.title("Political Party vs. Votes with Party")
     ax.set_xticks(x)
@@ -86,18 +109,39 @@ def display_line_graph (response):
     fig.tight_layout()
     plt.show()
 
+#user interface requiring int input between 101 - 117.
 
+print("Welcome to Congressional Voting Data""\n""\n"
+"This program is designed to utilize ProPublica API and pull voting statistics on senators.""\n" 
+"Specifically, it examines the percentage that members of the Senate vote with their Party." "\n"
+"Output includes:""\n" 
+"1. .csv of senators and their in line party voting percentavge," "\n" 
+"2. Line graph comparing each party by session of Congress and the percentage each party on average votes together,""\n" 
+"3. Scatter plot for the session of the Senate you request that breaks down by party and seniority voting trends." "\n")
+user_input = input("Please enter the session of the senate (101 - 117) you wish to view.  ")
+user_input = int(user_input)
+
+
+#Calling Functions
+
+get_congress_session(user_input)
+
+get_CSV_data(get_congress_session(user_input))
+
+display_line_graph(get_congress_session(user_input))
+
+display_scatter_plot(get_congress_session(user_input))
 #demonstrate when Democrats hold majority
-congressional_session_117 = get_congress_session(117)
+#congressional_session_117 = get_congress_session(117)
 #display_scatter_plot(congressional_session_117)
 
-congressional_session_115 = get_congress_session(115)
+#congressional_session_115 = get_congress_session(115)
 #demonstrate when Republicans hold Majority
 #display_scatter_plot(congressional_session_115)
 
-get_CSV_data(congressional_session_117)
-display_line_graph(congressional_session_117)
-display_scatter_plot(congressional_session_117)
+#get_CSV_data(congressional_session_117)
+#display_line_graph(congressional_session_117)
+#display_scatter_plot(congressional_session_117)
 
 #Cynical view:  if in minority, consequences for bucking the party are minor 
 #Slighty less cynical:  if in minority, legislation that comes to the vote is from the majority party and depending on vote, opposition is preferred in order to appease local constituents 
